a571cedbf058a8cc04833c81faddd7a016c08604
anastasia_wf <36860962+Anastasiawf@users.noreply.github.com>
2019-04-21 23:14:43 +0300

    Update template.erl

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -1,6 +1,21 @@
 -module(template).
+-export([parse/2, init/0]).
 
--export([parse/2]).
+init() -> In = <<"User {{name}} won {{wins}} games and got {{points}} points">>,
+Data = #{<<"name">> => "Kate", <<"wins">> => 55, <<"points">> => 777},
+{In, Data}.
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+Split_List = binary:split(Str, [<<"{{">>], [global]),
+Change_List = lists:map(fun(Split_List_2) ->
+case binary:split(Split_List_2, [<<"}}">>]) of
+[Change_N] -> Change_N;
+[Change | Rest] -> case maps:find(Change, Data) of
+error -> Rest;
+{ok, Value} when is_binary(Value) orelse is_list(Value) -> [Value, Rest];
+{ok, Value} when is_integer(Value) -> [integer_to_binary(Value), Rest]
+end
+end
+end,
+Split_List),
+unicode:characters_to_binary(Change_List).
